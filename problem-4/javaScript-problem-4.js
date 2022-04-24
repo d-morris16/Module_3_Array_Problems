b5e7c117f469ea2b8a2cbca8a6a4b8f18dab983d
Dominique Morris <morrisdominique16@yahoo.com>
2022-04-24 17:37:16 -0400

    Problem 4 submission

---
+++ problem-4/javaScript-problem-4.js
@@ -0,0 +1,18 @@
+//Write a JavaScript function to remove duplicate items from an array 
+
+/*
+Step 1: Create a function that will take in an array of elements. 
+Step 2: Create an array of elements.
+Step 3: Use the filter() method to reduce the duplicate items from the array. 
+*/
+
+const arrayOfStrings = ["Rock", "Paper", "Scissors", "Rock", "Paper", "Paper"];
+
+function arrayFilter(array) {
+  //We arrow function into the indexOf function to have it only accept the first 
+  //occurance of the array element.
+  console.log(array.filter((item, index) => array.indexOf(item) === index));
+  return array;
+}
+              
+arrayFilter(arrayOfStrings);
